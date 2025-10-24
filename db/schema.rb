31720d661946d18e4c4b3b1cf800b7f74c7946af
ponyo728 <yutota0728@gmail.com>
2025-10-24 11:47:51 +0900

    room_usersテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_10_23_043222) do
+ActiveRecord::Schema[7.1].define(version: 2025_10_23_045831) do
+  create_table "room_users", charset: "utf8mb3", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "rooms", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
